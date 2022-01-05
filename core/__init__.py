e6a2d6a61d0a88a5ce58379d9ccc0296fea2c605
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-01-05 08:26:03 +0700

    first commit

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+# this lstm core module implementation provides an implementation
+# of time series prediction using a lstm approach. It is provided
+# as is with no warranties or support.
+
+__author__ = "DungCW"
+__copyright__ = "Copyright 2021, DungCW"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import warnings
+warnings.filterwarnings("ignore")  # ignore messy numpy warnings
